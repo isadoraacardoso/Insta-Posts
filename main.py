846e426af070dd6bb19beca3ad4a8c5f4af7ee6f
Isadora Cardoso <101332900+isadoraacardoso@users.noreply.github.com>
2025-08-25 22:50:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ profile_name = input("Digite o nome do perfil: ")
 
 try:
     profile = instaloader.Profile.from_username(L.context, profile_name)
-except intaloader.exceptions.ProfileNoExistsException:
+except instaloader.exceptions.ProfileNoExistsException:
     print("Perfil não encontrado")
     exit()
 
